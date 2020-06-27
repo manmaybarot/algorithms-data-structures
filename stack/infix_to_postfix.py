d20390fbff7cfdce35a1c5e1eabe5888736b72e5
manmaybarot <manmaybarot@gmail.com>
2020-06-26 21:34:46 -0700

    infix to prefix, eval postfix,  fix postfix,

--- stack/infix_to_postfix.py
+++ stack/infix_to_postfix.py
@@ -37,7 +37,7 @@ def convert_infix_to_postfix(infix):
 
 
 if __name__ == '__main__':
-    infix = '5+23-5-(4-2)+(3-5(3+0))-12'
-    infix = '(A + B) * (C + D)'
-    infix = 'K+L-M*N+(O^P)*W/U/V*T+Q'
+    infix = '5+23-5-(4-2)*(3^5(3+0))/12'
+    # infix = '(A + B) * (C + D)'
+    # infix = 'K+L-M*N+(O^P)*W/U/V*T+Q'
     print(convert_infix_to_postfix(infix))
