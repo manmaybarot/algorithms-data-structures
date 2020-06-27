0538db59ff8900625ea4871b03114b6b8a30ca0a
manmaybarot <manmaybarot@gmail.com>
2020-06-26 21:58:38 -0700

    prefix2infix, postfix2infix

--- stack/eval_postfix.py
+++ stack/eval_postfix.py
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     postfix = '523+5-42-3530+^*12/-'
     print(eval_postfix(postfix))
 
-# For prefix eval, got right to left :)
+# For prefix eval, go right to left :)
