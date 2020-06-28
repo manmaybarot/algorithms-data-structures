defc47ffeab443d3bed6e7d36e8c29e830944abf
manmaybarot <manmaybarot@gmail.com>
2020-06-28 15:47:59 -0700

    construct bt from pre and post

--- tree/construct_bt.py
+++ tree/construct_bt.py
@@ -58,3 +58,35 @@ def build_bt_from_inorder_postorder(inorder, postorder):
         return root
 
     return build(0, len(postorder)-1)
+
+
+def build_bt_from_pre_post(pre, post):
+    if not pre: return
+    seen = {pre[0]}
+
+    post_index_of, pre_index_of = {}, {}
+    for i in range(len(post)):
+        post_index_of[post[i]] = i
+        pre_index_of[pre[i]] = i
+
+    def build(root):
+        root_value = root.val
+        print(root_value)
+        possible_left = pre_index_of[root_value] + 1
+        if possible_left < len(pre):
+            if pre[possible_left] not in seen:
+                seen.add(pre[possible_left])
+                root.left = TreeNode(pre[possible_left])
+
+        possible_right = post_index_of[root_value] - 1
+        if possible_right >= 0 :
+            if post[possible_right] not in seen:
+                seen.add(post[possible_right])
+                root.right = TreeNode(post[possible_right])
+
+        if root.left: build(root.left)
+        if root.right: build(root.right)
+
+        return root
+
+    return build(TreeNode(pre[0]))
