b1db51b8a6d706834433f93b00af6b1e9d2966ce
manmaybarot <manmaybarot@gmail.com>
2020-07-13 22:51:33 -0700

    veify preorder

---
+++ tree/verify_preorder.py
@@ -0,0 +1,21 @@
+# verify if preorder for vinary search tree is valid:
+
+from typing import List
+
+def verifyPreorder(self, preorder: List[int]) -> bool:
+    if not preorder: return True
+    current = 0
+
+    def verify(minimum, maximum):
+        nonlocal current
+        if current == len(preorder):
+            return True
+
+        root_val = preorder[current]
+
+        if root_val < minimum or root_val > maximum:
+            return False
+        current += 1
+        return verify(minimum, root_val-1) or verify(root_val+1, maximum)
+
+    return verify(float('-inf'), float('inf'))
