17ec5cabaa05d306c3d8877651ca2aa8c0db8d62
manmaybarot <manmaybarot@gmail.com>
2020-10-02 00:33:24 -0700

    lcs dp way

--- dynamic_programming/lcs.py
+++ dynamic_programming/lcs.py
@@ -3,7 +3,8 @@
 # Ref: https://leetcode.com/problems/longest-common-subsequence/
 
 
-def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+def longestCommonSubsequence_memo(self, text1: str, text2: str) -> int:
     dp = {}
     ans = ''
 
@@ -28,3 +29,19 @@ def longestCommonSubsequence(self, text1: str, text2: str) -> int:
             return max(ans1, ans2)
 
     return find_lcs(len(text1)-1, len(text2)-1)
+
+
+def longestCommonSubsequence_dp(text1: str, text2: str) -> int:
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+
+        for row in range(1, len(text1) + 1):
+            for col in range(1, len(text2) + 1):
+                r = row - 1
+                c = col - 1
+
+                if text1[r] == text2[c]:
+                    dp[row][col] = 1 + dp[row-1][col-1]
+                else:
+                    dp[row][col] = max(dp[row][col-1], dp[row-1][col])
+
+        return dp[-1][-1]
