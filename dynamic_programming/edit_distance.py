3ee629215f5f6a8798df6be46e125940ac65e761
manmaybarot <manmaybarot@gmail.com>
2020-10-13 22:25:25 -0700

    add minDistance_dp

--- dynamic_programming/edit_distance.py
+++ dynamic_programming/edit_distance.py
@@ -26,3 +26,25 @@ class Solution:
 
         return levenshtien(len(word1)-1, len(word2)-1)
 
+    def minDistance_dp(self, word1: str, word2: str) -> int:
+        table = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
+
+        for i in range(len(table[0])):
+            table[0][i] = i
+
+        for i in range(len(table)):
+            table[i][0] = i
+
+        cols = len(word2) + 1
+        rows = len(word1) + 1
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                w1 = row - 1
+                w2 = col - 1
+                if word1[w1] == word2[w2]:
+                    table[row][col] = table[row-1][col-1]
+                else:
+                    table[row][col] = 1 + min(table[row-1][col-1], table[row][col-1], table[row-1][col])
+
+        return table[-1][-1]
